2ffb1f3b21efdd3f7d7b0c6f605a67461fdf9bb4
nejiko96 <nejiko2006@gmail.com>
2016-10-27 15:12:36 +0900

    マウスリスナーの実装

--- src/app/minesweeper/minesweeper.js
+++ src/app/minesweeper/minesweeper.js
@@ -55,48 +55,16 @@ const styles = {
   restart: {}
 };
 
-const settings = {
-  levels: {
-    easy: {
-      width: 9,
-      height: 9,
-      mines: 10
-    },
-    medium: {
-      width: 16,
-      height: 16,
-      mines: 40
-    },
-    hard: {
-      width: 30,
-      height: 16,
-      mines: 99
-    }
-  },
-  width: {
-    min: 9,
-    max: 30,
-    default: 9
-  },
-  height: {
-    min: 9,
-    max: 24,
-    default: 9
-  },
-  mines: n => {
-    const pct = 10 + Math.floor(n / 45);
-    return {
-      min: 10,
-      max: Math.floor(n * 0.94 - 8.45),
-      default: Math.round(n * pct / 1000) * 10
-    };
-  }
-};
-
 const utils = {
   getProperty: (value, opt) => value ? Math.min(Math.max(value, opt.min), opt.max) : opt.default,
   fillArray2D: (w, h, value) => Array.apply(null, Array(h)).map(() => Array(w).fill(value)),
-  noop: () => {}
+  noop: () => {},
+  addEventListener: (cmp, event, fn) => {
+    ReactDOM.findDOMNode(cmp).addEventListener(event, fn);
+  },
+  removeEventListener: (cmp, fn) => {
+    ReactDOM.findDOMNode(cmp).removeEventListener(event, fn);
+  }
 };
 
 class Remain extends Component {
@@ -112,14 +80,16 @@ Remain.propTypes = {
 };
 
 class Timer extends Component {
-  timeParse(value) {
-    const powers = {
+  static get powers() {
+    return {
       ms: 1,
       s: 1000
     };
+  }
+  timeParse(value) {
     const result = /^([0-9]+(?:\.[0-9]*)?)\s*(.*s)?$/.exec(value.trim());
     const num = result[1] && parseFloat(result[1]) || 1000;
-    const mult = result[2] && powers[result[2]] || 1;
+    const mult = result[2] && Timer.powers[result[2]] || 1;
     return num * mult;
   }
   start() {
@@ -134,10 +104,12 @@ class Timer extends Component {
   }
   stop() {
     clearInterval(this.intervalID);
+    this.intervalID = null;
   }
   constructor(props) {
     super(props);
     this.state = {count: 0};
+    this.intervalID = null;
     // event binding
     this.timeParse = this.timeParse.bind(this);
     this.start = this.start.bind(this);
@@ -145,17 +117,14 @@ class Timer extends Component {
     this.stop = this.stop.bind(this);
   }
   componentDidMount() {
-    if (this.props.running) {
-      this.start();
-    }
+    this.props.running && this.start();
+  }
+  componentWillUnmount() {
+    this.props.running && this.intervalID && this.stop();
   }
   componentWillReceiveProps(nextProps) {
     if (this.props.running !== nextProps.running) {
-      if (nextProps.running) {
-        this.start();
-      } else {
-        this.stop();
-      }
+      nextProps.running ? this.start() : this.stop();
     }
   }
   render() {
@@ -172,6 +141,14 @@ Timer.propTypes = {
 };
 
 class Listener {
+  static get event() {
+    return {
+      mouseDown: 0,
+      mouseUp: 1,
+      mouseOver: 2,
+      mouseOut: 3
+    };
+  }
   static get button() {
     return {
       left: 1,
@@ -179,27 +156,77 @@ class Listener {
       both: 3
     };
   }
-  static get which() {
-    return {
-      left: 1,
-      right: 3
-    };
-  }
   constructor(target) {
-    this.target = target;
-    this.button
+    this.pressed = 0;
+    this.callbacks = [
+      [
+        target.handleLeftMouseDown,
+        target.handleRightMouseDown,
+        target.handleBothMouseDown,
+      ],
+      [
+        target.handleLeftMouseUp,
+        target.handleRightMouseUp,
+        target.handleBothMouseUp,
+      ],
+      [
+        target.handleLeftMouseOver,
+        target.handleRightMouseOver,
+        target.handleBothMouseOver,
+      ],
+      [
+        target.handleLeftMouseOut,
+        target.handleRightMouseOut,
+        target.handleBothMouseOut,
+      ]
+    ];
   }
   handleMouseDown(ev, i, j) {
-    console.log(`MouseDown(${i}, ${j}) : button=[${ev.button}], which=[${ev.which}]`);
+    this.pressed |= {
+      0: Listener.button.left,
+      2: Listener.button.right
+    }[ev.button];
+    this.triggerCallback(
+      Listener.event.mouseDown,
+      this.pressed,
+      i, j
+    );
   }
-  handleMouseUp(ev, i, j) {
-    console.log(`MouseUp(${i}, ${j}) : button=[${ev.button}], which=[${ev.which}]`);
+  handleMouseUp(i, j) {
+    if (this.pressed === 0) {
+      return;
+    }
+    const pressed = this.pressed;
+    this.pressed = 0;
+    this.triggerCallback(
+      Listener.event.mouseUp,
+      pressed,
+      i, j
+    );
   }
-  handleMouseOver(ev, i, j) {
-    console.log(`MouseOver(${i}, ${j})`);
+  handleMouseOver(i, j) {
+    if (this.pressed === 0) {
+      return;
+    }
+    this.triggerCallback(
+      Listener.event.mouseOver,
+      this.pressed,
+      i, j
+    );
   }
-  handleMouseOut(ev, i, j) {
-    console.log(`MouseOut(${i}, ${j})`);
+  handleMouseOut(i, j) {
+    if (this.pressed === 0) {
+      return;
+    }
+    this.triggerCallback(
+      Listener.event.mouseOut,
+      this.pressed,
+      i, j
+    );
+  }
+  triggerCallback(e, b, i, j) {
+    const cb = this.callbacks[e][b - 1];
+    cb && cb(i, j);
   }
 }
 
@@ -230,25 +257,11 @@ class Board extends Component {
       hasFlag: 2
     };
   }
-  defaultSize(level) {
-    return settings.levels[level];
-  }
-  customSize(props) {
-    const w = utils.getProperty(props.width, settings.width);
-    const h = utils.getProperty(props.height, settings.height);
-    const m = utils.getProperty(props.mines, settings.mines(w * h));
-    return {
-      width: w,
-      height: h,
-      mines: m
-    };
-  }
   init(props) {
-    const size = this.defaultSize(props.level) || this.customSize(props);
-    const w = size.width;
-    const h = size.height;
-    const other = {
-      remain: size.mines,
+    const w = props.width;
+    const h = props.height;
+    return {
+      remain: props.mines,
       cells: utils.fillArray2D(w, h, Board.cellTypes.notMarked),
       flags: utils.fillArray2D(w, h, 0)
     };
@@ -274,8 +287,6 @@ class Board extends Component {
   }
   componentDidMount() {
     this.listener = new Listener(this);
-    // this.listener = MinesweeperListener.noop;
-    this.props.onRemainChange(this.state.remain);
   }
   render() {
     const boardNodes = this.state.cells.map((row, i) => {
@@ -286,9 +297,9 @@ class Board extends Component {
             key={key}
             style={styles.cell[cell]}
             onMouseDown={(ev) => this.listener.handleMouseDown(ev, i, j)}
-            onMouseUp={(ev) => this.listener.handleMouseUp(ev, i, j)}
-            onMouseOver={(ev) => this.listener.handleMouseOver(ev, i, j)}
-            onMouseOut={(ev) => this.listener.handleMouseOut(ev, i, j)}
+            onMouseUp={() => this.listener.handleMouseUp(i, j)}
+            onMouseOver={() => this.listener.handleMouseOver(i, j)}
+            onMouseOut={() => this.listener.handleMouseOut(i, j)}
             />);
       });
       rowNodes.push(<br/>);
@@ -300,41 +311,113 @@ class Board extends Component {
       </div>
     );
   }
-
-}
+  handleLeftMouseDown(i, j) {
+    console.log(`LeftMouseDown(${i}, ${j})`);
+  }
+  handleLeftMouseUp(i, j) {
+    console.log(`LeftMouseUp(${i}, ${j})`);
+  }
+  handleRightMouseDown(i, j) {
+    console.log(`RightMouseDown(${i}, ${j})`);
+  }
+  handleRightMouseUp(i, j) {
+    console.log(`RightMouseUp(${i}, ${j})`);
+  }
+  handleBothMouseDown(i, j) {
+    console.log(`BothMouseDown(${i}, ${j})`);
+  }
+  handleBothMouseUp(i, j) {
+    console.log(`BothMouseUp(${i}, ${j})`);
+  }}
 
 Board.propTypes = {
   gameId: React.PropTypes.number.isRequired,
-  level: React.PropTypes.string.isRequired,
-  width: React.PropTypes.number,
-  height: React.PropTypes.number,
-  mines: React.PropTypes.number,
+  width: React.PropTypes.number.isRequired,
+  height: React.PropTypes.number.isRequired,
+  mines: React.PropTypes.number.isRequired,
   onStart: React.PropTypes.func,
   onStop: React.PropTypes.func,
   onRemainChange: React.PropTypes.func
 };
 
 export class Minesweeper extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+  static get settings() {
+    return {
+      levels: {
+        easy: {
+          width: 9,
+          height: 9,
+          mines: 10
+        },
+        medium: {
+          width: 16,
+          height: 16,
+          mines: 40
+        },
+        hard: {
+          width: 30,
+          height: 16,
+          mines: 99
+        }
+      },
+      width: {
+        min: 9,
+        max: 30,
+        default: 9
+      },
+      height: {
+        min: 9,
+        max: 24,
+        default: 9
+      },
+      mines: n => {
+        const pct = 10 + Math.floor(n / 45);
+        return {
+          min: 10,
+          max: Math.floor(n * 0.94 - 8.45),
+          default: Math.round(n * pct / 1000) * 10
+        };
+      }
+    };
+  }
+  defaultSize(level) {
+    return Minesweeper.settings.levels[level || 'easy'];
+  }
+  customSize(props) {
+    const w = utils.getProperty(props.width, Minesweeper.settings.width);
+    const h = utils.getProperty(props.height, Minesweeper.settings.height);
+    const m = utils.getProperty(props.mines, Minesweeper.settings.mines(w * h));
+    return {
+      width: w,
+      height: h,
+      mines: m
+    };
+  }
+  init(props) {
+    const size = this.defaultSize(props.level) || this.customSize(props);
+    const other = {
       gameId: 1,
       running: false,
-      remain: 0
+      remain: size.mines
     };
+    return Object.assign({}, size, other);
+  }
+  constructor(props) {
+    super(props);
+    this.state = this.init(props);
     // event binding
     this.handleStart = this.handleStart.bind(this);
     this.handleStop = this.handleStop.bind(this);
-    this.handleRemainChange = this.handleRemainChange.bind(this);
+    this.handleBoardChange = this.handleBoardChange.bind(this);
     this.handleRetry = this.handleRetry.bind(this);
   }
   componentDidMount() {
-    ReactDOM.findDOMNode(this).addEventListener('contextmenu', this.handleContextMenu);
-    ReactDOM.findDOMNode(this).addEventListener('selectstart', this.handleSelectStart);
+    utils.addEventListener(this, 'contextmenu', this.handleContextMenu);
+    utils.addEventListener(this, 'selectstart', this.handleSelectStart);
   }
   componentWillUnmount() {
-    ReactDOM.findDOMNode(this).removeEventListener('contextmenu', this.handleContextMenu);
-    ReactDOM.findDOMNode(this).removeEventListener('selectstart', this.handleSelectStart);
+    utils.removeEventListener(this, 'contextmenu', this.handleContextMenu);
+    utils.removeEventListener(this, 'selectstart', this.handleSelectStart);
   }
   render() {
     return (
@@ -346,13 +429,12 @@ export class Minesweeper extends Component {
             time : <Timer interval="1s" limit={999} running={this.state.running}/>
             <Board
               gameId={this.state.gameId}
-              level={this.props.level || 'easy'}
-              width={this.props.width}
-              height={this.props.height}
-              mines={this.props.mines}
+              width={this.state.width}
+              height={this.state.height}
+              mines={this.state.mines}
               onStart={this.handleStart}
               onStop={this.handleStop}
-              onRemainChange={this.handleRemainChange}
+              onChange={this.handleBoardChange}
               />
             <button
               type="button"
@@ -370,7 +452,7 @@ export class Minesweeper extends Component {
   handleStop() {
     this.setState({running: false});
   }
-  handleRemainChange(value) {
+  handleBoardChange(value) {
     this.setState({remain: value});
   }
   handleRetry() {
